fe8842059b251b0aa4a9de9b3d8d2ff66d1e3fc5
Dan <danielpinhodovale@gmail.com>
2023-09-20 15:20:39 -0300

    pegando dados cadastro

--- registro/app1/views.py
+++ registro/app1/views.py
@@ -8,6 +8,14 @@ def Home(request):
 
 
 def SignUp(request):
+    if request.method == 'POST':
+        uname = request.POST.get('username')
+        email = request.POST.get('email')
+        pass1 = request.POST.get('password1')
+        pass2 = request.POST.get('password2')
+
+        print(uname, email, pass1, pass2)
+
     return render(request, 'signup.html')
 
 
