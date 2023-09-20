bf81f98a6bf243005f67a1d3eca89eed999fe340
Dan <danielpinhodovale@gmail.com>
2023-09-20 14:34:25 -0300

    Linkado urls-views Signup

--- registro/app1/views.py
+++ registro/app1/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def Home(request):
+    pass
+
+
+def SignUp(request):
+    return render(request, 'signup.html')
+
+
+def Login(request):
+    pass
